5a718fabda2462e20d08161725e3b273558b17ca
Jason <ozil.liu2@gmail.com>
2018-07-28 20:45:17 +0800

    init

---
+++ src/main/java/com/jwebcoder/groceryauth/GroceryAuthApplication.java
@@ -0,0 +1,12 @@
+package com.jwebcoder.groceryauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryAuthApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jwebcoder/groceryauth/GroceryAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jwebcoder.groceryauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroceryAuthApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
