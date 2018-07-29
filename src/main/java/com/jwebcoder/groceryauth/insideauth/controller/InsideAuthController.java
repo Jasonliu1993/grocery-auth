5b557aeff94ec822d243dc9ce1a823821be03d8c
Jason <ozil.liu2@gmail.com>
2018-07-29 19:29:55 +0800

    系统内校验流程重构

--- src/main/java/com/jwebcoder/groceryauth/insideauth/controller/InsideAuthController.java
+++ src/main/java/com/jwebcoder/groceryauth/insideauth/controller/InsideAuthController.java
@@ -4,25 +4,27 @@ import com.jwebcoder.groceryauth.common.dto.SystemUserDTO;
 import com.jwebcoder.groceryauth.common.dto.AuthInfoDTO;
 import com.jwebcoder.groceryauth.insideauth.service.InsideAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("insideAuth")
+@RequestMapping("/insideAuth")
 public class InsideAuthController {
 
     @Autowired
     private InsideAuthService insideAuthService;
 
+    @PostMapping("/login")
     SystemUserDTO loginAuthentication(String userName, String password) {
         return insideAuthService.loginAuthentication(userName, password);
     }
 
+    @PostMapping("/register")
     SystemUserDTO registerAuthentication(String userName, String password4Register, String activeEmail) {
         return insideAuthService.registerAuthentication(userName, password4Register, activeEmail);
     }
 
-    AuthInfoDTO doAuthInfo(String object, String content) {
+    @GetMapping("/registerCheck")
+    AuthInfoDTO doAuthInfo(@RequestParam String object, @RequestParam String content) {
         return insideAuthService.doAuthInfo(object, content);
     }
 
