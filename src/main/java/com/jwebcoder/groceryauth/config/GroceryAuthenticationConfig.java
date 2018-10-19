d94f377076b95f8b5c9896e27694b2f2d337d982
Jason <ozil.liu2@gmail.com>
2018-10-19 17:59:33 +0800

    auth配置成功

--- src/main/java/com/jwebcoder/groceryauth/config/GroceryAuthenticationConfig.java
+++ src/main/java/com/jwebcoder/groceryauth/config/GroceryAuthenticationConfig.java
@@ -1,43 +1,25 @@
 package com.jwebcoder.groceryauth.config;
 
-import com.jwebcoder.groceryauth.common.service.GroceryCodeAuthService;
-import com.jwebcoder.groceryauth.common.service.MyBCryptPasswordEncoder;
 import com.jwebcoder.groceryauth.common.service.MyRedisTokenStore;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authorization.ReactiveAuthorizationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.security.KeyPair;
 
 @Component
 public class GroceryAuthenticationConfig extends AuthorizationServerConfigurerAdapter {
@@ -58,12 +40,6 @@ public class GroceryAuthenticationConfig extends AuthorizationServerConfigurerAd
         return new MessageDigestPasswordEncoder("MD5");
 //        return new MyBCryptPasswordEncoder();
     }
-
-   /*
-    @Autowired
-    private ClientDetailsService clientDetailsService;
-   */
-
 
     @Bean
     public MyRedisTokenStore tokenStore() {
